417d17313a2020436156920d08ebed002b47c9e0
Sai Mahendra <118994059+saimahendra282@users.noreply.github.com>
2024-04-08 19:13:58 +0530

    Add files via upload

---
+++ bharatinternprofile/dist/app.dev.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+(function () {
+  _toConsumableArray(document.querySelectorAll(".control")).forEach(function (button) {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
+    });
+  });
+
+  document.querySelector(".theme-btn").addEventListener("click", function () {
+    document.body.classList.toggle("light-mode");
+  });
+})();
+//# sourceMappingURL=app.dev.js.map
